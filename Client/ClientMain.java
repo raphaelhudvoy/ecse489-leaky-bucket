a8fed21d1f35c3e7657b69989ea4ac1f0101202e
git <rhvoyer@vpn164116.wireless.mcgill.ca>
2015-03-30 11:20:47 -0400

    client started

--- Client/ClientMain.java
+++ Client/ClientMain.java
@@ -1,9 +1,20 @@
 package Client;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class ClientMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			// Connecting to the server
+			Socket client = new Socket(Config.HOSTNAME, Config.PORT);
+
+			
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
---
+++ Client/Config.java
@@ -0,0 +1,8 @@
+package Client;
+
+public class Config {
+	
+	public static final int PORT = 5005;
+	public static final String HOSTNAME = "localhost";
+
+}
