cbabd9c6b2a264dd1db33c05113d2d97bdb603f8
Clark Gredoña <cgredona@gmail.com>
2015-03-30 14:40:44 -0400

    Add packet size to server config

--- Server/ServerConfig.java
+++ Server/ServerConfig.java
@@ -4,11 +4,12 @@ public class ServerConfig {
 	
 	public int capacity = -1;
 	public int rate = -1;
+	public int packetSize = -1;
 
 	public ServerConfig(String[] commandLineArgs) {
-		if (commandLineArgs.length < 4) {
+		if (commandLineArgs.length < 6) {
 			throw new IllegalArgumentException("Not enough arguments" +
-				"\nFormat : ServerMain -c [capacity] -r [leak rate]");
+				"\nFormat : ServerMain -c [capacity] -r [leak rate] -p [packet size]");
 		}	
 		
 		for (int i = 0; i < commandLineArgs.length - 1; i++) {
@@ -30,7 +31,18 @@ public class ServerConfig {
 			}
 		}
 		
-		if (capacity < 0 || rate < 0) {
+		for (int i = 0; i < commandLineArgs.length - 1; i++) {
+			if (commandLineArgs[i].equals("-p")) {
+				packetSize = Integer.parseInt(commandLineArgs[i + 1]);				
+				if (packetSize <= 0) {
+					throw new IllegalArgumentException("Packet size must be positive");
+				}
+				break;
+			}
+		}
+		
+		
+		if (capacity < 0 || rate < 0 || packetSize < 0) {
 			throw new RuntimeException("Unable to parse arguments");
 		}
 		
